a657a605493042ab1e72b249ebf3f33b353c0bfa
Jonathan Daniel <gcagrad19@gmail.com>
2023-02-07 20:16:43 -0500

    Added Watt Logo

--- src/Components/Homepage/Partners/Partners.js
+++ src/Components/Homepage/Partners/Partners.js
@@ -5,6 +5,7 @@ import ge from './new/ge_monogram_dark gray_432.png';
 import partnerdevice from './new/cactus.png';
 import LPL from "./new/LPL.png";
 import Postman from "./new/postman.svg";
+import logo_watt from './logo_watt.png';
 
 ge ? console.log(ge): console.log("no dice");
 
@@ -16,7 +17,8 @@ function Partners() {
         <div className="partnertextbody">
           <h2 className="partnerhead">in partnership with</h2>
           <div className="thepartner">
-            <a href="https://www.clemson.edu/cecas/departments/computing/" target="_blank"><div><img src={logo} alt="GE"/></div></a>
+            <a href="https://www.clemson.edu/cecas/departments/computing/" target="_blank"><div><img src={logo} alt="Clemson University School of Computing Logo"/></div></a>
+            <a href="https://www.clemson.edu/centers-institutes/watt/" target="_blank"><div><img src={logo_watt} alt="Watt Family Innovation Center Logo"/></div></a>
             <a href="https://aws.amazon.com/" target="_blank"><div><img src={aws_logo} alt="AWS Logo"/></div></a>
             <a href="https://www.lpl.com" target="_blank"><div><img src={LPL} alt="LPL Logo"/></div></a>
             <a href="https://www.postman.com" target="_blank"><div><img src={Postman} alt="Postman Logo"/></div></a>
